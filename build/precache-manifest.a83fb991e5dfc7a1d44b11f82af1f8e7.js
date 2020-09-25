46a4812db37fd83efe3db10107eb50d45de9a579
Casey The Great <casey@caseypitman.com>
2020-09-25 13:46:30 -0500

    Attempted sticky focus fix for touch devices.

---
+++ build/precache-manifest.a83fb991e5dfc7a1d44b11f82af1f8e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd5b0b4d4a8f0c5f7dc62b6eeae24d0",
+    "url": "/quiz1/index.html"
+  },
+  {
+    "revision": "9335f7114fcf525e1c26",
+    "url": "/quiz1/static/css/main.7b5c808b.chunk.css"
+  },
+  {
+    "revision": "3831b5ebbc7a1589a587",
+    "url": "/quiz1/static/js/2.292b840e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz1/static/js/2.292b840e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9335f7114fcf525e1c26",
+    "url": "/quiz1/static/js/main.2b1c6bc8.chunk.js"
+  },
+  {
+    "revision": "fb231f7a1a9780e9dd66",
+    "url": "/quiz1/static/js/runtime-main.608fc375.js"
+  }
+]);
